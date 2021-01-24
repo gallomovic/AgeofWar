aba61bfd4ae2baafd5d745f0ae47b8fae3053379
gallomovic <64538176+gallomovic@users.noreply.github.com>
2021-01-24 21:42:49 +0100

    Update Playturn.cpp

--- AgeofWar/Playturn.cpp
+++ AgeofWar/Playturn.cpp
@@ -1,10 +1,12 @@
 //Pas besoin de header vu que playturn n'a que des fonctions qu'on appelera dans le main
-
+#include "Playground.h"
+#include "Player.h"
 
 void Playturn(Playground* p, Player *p1, Player *p2) {
 
-
         //TODO :)
+        //si p1 action 1, 2 et 3 ok (+ nvlles unités) --> p2 et inversement
+        
 
 
 
